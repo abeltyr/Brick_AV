34d3d3b34185cb1a9f498dda7f22b52ab59979e4
abeltyr <abeltyr@gmail.com>
2024-08-23 17:37:49 +0300

    (feat) employee and contractor payroll and tax setup

--- prisma/seed/runs/working/setup.ts
+++ prisma/seed/runs/working/setup.ts
@@ -1,26 +1,31 @@
 import { PrismaClient } from "@prisma/client";
 
 export const runSetupSeed = async (prisma: PrismaClient) => {
-  const company = await prisma.company.create({
-    data: {
-      userName: "eurka",
-      name: "Eurka",
-    },
-  });
-
-  const user = await prisma.user.create({
-    data: {
-      email: "abel@texly.co",
-      name: "Abel",
-      id: "74387e66-0125-484c-a7be-21a6a3992ab3",
-    },
-  });
-
-  const companyUser = await prisma.companyUser.create({
-    data: {
-      userId: "74387e66-0125-484c-a7be-21a6a3992ab3",
-      companyId: company.id,
-      role: "Owner",
-    },
-  });
+  // const company = await prisma.company.create({
+  //   data: {
+  //     legalName: "eurka",
+  //     name: "Eurka",
+  //   },
+  // });
+  // const user = await prisma.user.create({
+  //   data: {
+  //     email: "abel@texly.co",
+  //     name: "Abel",
+  //     id: "74387e66-0125-484c-a7be-21a6a3992ab3",
+  //   },
+  // });
+  // const companyUser = await prisma.companyUser.create({
+  //   data: {
+  //     userId: "74387e66-0125-484c-a7be-21a6a3992ab3",
+  //     companyId: company.id,
+  //     role: "Owner",
+  //   },
+  // });
+  // const company = await prisma.contractorPayroll.findMany({
+  //   where: {
+  //     contractor:{
+  //      companyId:id
+  //     }
+  //   },
+  // });
 };
